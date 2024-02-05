865c85e59c309a4bce33c97babf8d3424aef4152
PawelBis <bis1pawel1@gmail.com>
2024-02-05 01:32:24 +0100

    Remove boudns and is_child from BevyWry configuration.
    Webveiw is using 'new_as_child' by default, otherwise you may encounter
    some issues with transparency. Additionally new 'viewport' field was
    specified. This field will setup viewport camera.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 mod error;
 
-use bevy::{prelude::*, utils, window::PrimaryWindow, winit::WinitWindows};
+use bevy::{prelude::*, utils, window::PrimaryWindow, winit::WinitWindows, render::camera::Viewport};
 use error::Error;
-use wry::{raw_window_handle::HasRawWindowHandle, Rect, WebView, WebViewBuilder};
+use wry::{WebView, WebViewBuilder};
 
 type Result<T> = std::result::Result<T, Error>;
 
@@ -11,57 +11,26 @@ type Result<T> = std::result::Result<T, Error>;
 #[derive(Resource, Deref, Clone, Default)]
 pub struct UrlResource<const N: u8>(pub String);
 
-#[derive(Resource, Clone, Copy, Deref, Debug)]
-pub struct WebViewBounds(pub Rect);
-
-impl From<Rect> for WebViewBounds {
-    fn from(value: Rect) -> Self {
-        Self(value)
-    }
-}
-
-impl Default for WebViewBounds {
-    fn default() -> Self {
-        Rect {
-            x: 0,
-            y: 0,
-            width: 200,
-            height: 200,
-        }
-        .into()
-    }
-}
+#[derive(Resource, Clone, Default)]
+pub struct EditorViewport(pub Viewport);
 
 #[derive(Resource, Clone, Default)]
+/// Wry window is allways spawned as a child of `PrimaryWindow`, otherwise
+/// transparency in the webview will be broken.
 pub struct BevyWryPlugin {
-    /// WebView will be created as a child window if `as_child == true`
-    pub as_child: bool,
-    /// WebView will be initialised with this bounds.
-    /// Default: Rect { x: 0, y: 0, width: 200, height: 200 }
-    pub bounds: Option<WebViewBounds>,
     /// WebView will be initialised with this url
     /// Additionally it will be stored via `insert_resource`
     pub url: UrlResource<0>,
+    /// `bevy_wry` will setup a viewport camera if `viewport.is_some()`. This
+    /// viewport can be modified through `EditorViewport` resource.
+    pub viewport: Option<Viewport>,
 }
 
 impl Plugin for BevyWryPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(self.clone())
             .init_non_send_resource::<Option<WebView>>()
-            .add_systems(Startup, setup_webview.map(utils::error))
-            .add_systems(Last, update_webview_bounds.after(setup_webview));
-    }
-}
-
-fn init_webview_builder<'a>(
-    wry_config: &BevyWryPlugin,
-    parent: &'a impl HasRawWindowHandle,
-) -> WebViewBuilder<'a> {
-    if wry_config.as_child {
-        WebViewBuilder::new_as_child(parent)
-            .with_bounds(*wry_config.bounds.clone().unwrap_or_default())
-    } else {
-        WebViewBuilder::new(parent)
+            .add_systems(Startup, setup_webview.map(utils::error));
     }
 }
 
@@ -79,20 +48,26 @@ fn setup_webview(world: &mut World) -> Result<()> {
         .get_window(primary_window_entity)
         .ok_or(Error::FailedToGetMainWindow)?;
 
-    let webview = init_webview_builder(&wry_config, primary_window)
+    let webview = WebViewBuilder::new_as_child(primary_window)
         .with_transparent(true)
         .with_url(&wry_config.url)?
         .build()?;
 
     world.insert_resource(wry_config.url);
-    world.insert_resource(wry_config.bounds.unwrap_or_default());
     world.insert_non_send_resource(webview);
+
+    if let Some(viewport) = wry_config.viewport {
+        let camera = Camera2dBundle {
+            camera: Camera {
+                viewport: Some(viewport),
+                ..default()
+            },
+            ..default()
+        };
+
+        world.spawn(camera);
+    }
 
     Ok(())
 }
 
-/// This system handles changes in webview bounds. Those changes can be schedules via `WebViewBounds` resource
-fn update_webview_bounds(bounds: Res<WebViewBounds>, webview: NonSend<WebView>) {
-    let bounds: WebViewBounds = *bounds;
-    webview.set_bounds(*bounds);
-}
