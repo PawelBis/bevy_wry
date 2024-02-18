3352ff2cae079cfa19ee374fbd6deb538469a1f2
PawelBis <bis1pawel1@gmail.com>
2024-02-18 10:18:24 +0100

    Remove unused enum Direction

--- src/communication.rs
+++ src/communication.rs
@@ -3,20 +3,6 @@ use std::sync::{Arc, Mutex};
 use bevy::prelude::{Deref, Event, Resource};
 use serde::{Deserialize, Serialize};
 
-#[repr(u8)]
-pub enum Direction {
-    In = 0,
-    Out = 1,
-}
-pub const IN: u8 = Direction::In as u8;
-pub const OUT: u8 = Direction::In as u8;
-
-impl From<Direction> for u8 {
-    fn from(v: Direction) -> Self {
-        v as u8
-    }
-}
-
 #[derive(Deref, Event, Serialize)]
 #[serde(transparent)]
 pub struct OutEvent<T: Event + Clone + Serialize>(pub T);
